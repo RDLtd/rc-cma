8728e095cbf7bf43732bb6bfe77978de39f66ee9
jmbrnrd <jmbarnrd@gmail.com>
2019-10-17 12:08:25 +0100

    Post-Breakfast bugfix

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -9,11 +9,13 @@ export class AboutComponent implements OnInit {
 
   build: any;
   os_text;
+  company_logo_root;
 
   constructor() {}
 
   ngOnInit() {
     // show the OS and browser versions
     this.os_text = navigator.appVersion;
+    this.company_logo_root = localStorage.getItem('rd_company_logo_root');
   }
 }
