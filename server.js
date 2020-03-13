5ec191d70ee4c3b0a2a161d4fd051833cff5f8db
kenstratford <ks@ongava.com>
2020-03-13 16:43:30 +0200

    Heroku support suggestion - 3

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const compression = require('compression');
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Start the app by listening on the default
