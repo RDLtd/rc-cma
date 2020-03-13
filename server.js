104de3becab3772dc35394bea44923dc77ffa9a0
kenstratford <ks@ongava.com>
2020-03-13 12:22:45 +0200

    Revised server.js

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const compression = require('compression');
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+app.use(express.static(path.join(__dirname, 'dist','app_name')));
+res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
 
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 // If an incoming request uses
 // a protocol other than HTTPS,
