c8a3398ba8e85d2e5f54e253d16f860871d6efae
jmbrnrd <jmbarnrd@gmail.com>
2020-06-05 13:49:37 +0100

    QR Download

--- src/app/cms/cms-spw-links.component.ts
+++ src/app/cms/cms-spw-links.component.ts
@@ -6,9 +6,25 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './cms-spw-links.component.html'
 })
 export class CmsSpwLinksComponent implements OnInit {
+  base64main: string;
+  base64menu: string;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {
+    // Delay creating QR Code downloads
+    // until QR Code Component returns
+    // No callback available
+    setTimeout(() => {
+      this.getQRCodes();
+    }, 1000);
   }
+
+  getQRCodes(): void {
+    const elems = document.querySelectorAll('qrcode');
+    this.base64main = elems[0].firstElementChild.firstElementChild.getAttribute('src');
+    this.base64menu = elems[1].firstElementChild.firstElementChild.getAttribute('src');
+  }
+
 
 }
