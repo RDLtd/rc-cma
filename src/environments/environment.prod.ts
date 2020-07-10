bb89efaedcb7fe70b307edd730c1ea9593d3303e
jmbrnrd <jmbarnrd@gmail.com>
2020-07-10 14:54:58 +0100

    Final pkg clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://rc-server-prod.herokuapp.com'
+  API_URL: 'https://dashboard.heroku.com/apps/rc-cma-production'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://rc-server-staging.herokuapp.com'
+  API_URL: 'https://dashboard.heroku.com/apps/rc-cma-staging'
 };
