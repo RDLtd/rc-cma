ea17373d62632f67dd7565015ee3ac67dc63c327
jmbrnrd <jmbarnrd@gmail.com>
2021-03-13 12:18:12 +0000

    Translation clean-up

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -37,10 +37,11 @@ export class JoinComponent implements OnInit {
     restaurant: null,
     promo_status: null
   };
-  t_data: any;
+  //t_data: any;
   patternMobile = '^([+\\d]\\d*)?\\d$';
   lang = localStorage.getItem('rd_language');
   stripeSessionId: any;
+  jobs: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -60,15 +61,16 @@ export class JoinComponent implements OnInit {
     });
 
     // Switch language
-    translate.onLangChange.subscribe(() => {
-      this.translate.get('Join').subscribe(data => {
-        this.t_data = data;
-      });
-    });
+    // translate.onLangChange.subscribe(() => {
+    //   this.translate.get('Join').subscribe(data => {
+    //     this.t_data = data;
+    //   });
+    // });
 
 
   }
   ngOnInit() {
+
     // Check for a referral code in the url
     this.route.paramMap
       .subscribe(params => {
@@ -80,15 +82,20 @@ export class JoinComponent implements OnInit {
           sessionStorage.setItem('referrer_type', 'self');
         }
       });
+
     // Set brand
     this.brand = this.config.brand;
-    // Get translations
-    this.translate.get('Join').subscribe(data => {
-      this.t_data = data;
-    });
+    // Get array of translated jobs
+    this.translate.get('JOIN.jobRoles').subscribe(jobs => this.jobs = jobs);
+
+    // // Get translations
+    // this.translate.get('Join').subscribe(data => {
+    //   this.t_data = data;
+    // });
+
     // Check session storage
-    this.pendingMember = JSON.parse(sessionStorage.getItem('rc_member_pending')) || {};
     // console.log('Pending Member', this.pendingMember);
+    this.pendingMember = JSON.parse(sessionStorage.getItem('rc_member_pending')) || {};
 
     // Watch for unload event (page refresh etc.)
     // Keep data in session storage
@@ -149,90 +156,90 @@ export class JoinComponent implements OnInit {
       });
   }
 
-  async submitJoinForm(applicant) {
-    this.currentApplicant = applicant;
-    this.isSubmitting = true;
+  // async submitJoinForm(applicant) {
+  //   this.currentApplicant = applicant;
+  //   this.isSubmitting = true;
+  //
+  //   this.load.open();
+  //
+  //   // TODO
+  //   // NB Note that for this early December version we set member_type to 'Full'
+  //   // Once there is a paywall we will set this to 'Payment Pending' and update once payment
+  //   // has been received
+  //   const admin = {
+  //     member_first_name: applicant.first_name,
+  //     member_last_name: applicant.last_name,
+  //     member_email: applicant.email,
+  //     member_telephone: applicant.mobile,
+  //     member_job: applicant.role,
+  //     member_language: localStorage.getItem('rd_language'),
+  //     restaurant_id: 0,
+  //     member_id: 0,
+  //     member_promo_status: this.referrer.promo_status,
+  //     member_type: 'Full'
+  //   };
+  //
+  //   // Register new admin
+  //   this.createContentAdministrator(admin);
+  // }
 
-    this.load.open();
-
-    // TODO
-    // NB Note that for this early December version we set member_type to 'Full'
-    // Once there is a paywall we will set this to 'Payment Pending' and update once payment
-    // has been received
-    const admin = {
-      member_first_name: applicant.first_name,
-      member_last_name: applicant.last_name,
-      member_email: applicant.email,
-      member_telephone: applicant.mobile,
-      member_job: applicant.role,
-      member_language: localStorage.getItem('rd_language'),
-      restaurant_id: 0,
-      member_id: 0,
-      member_promo_status: this.referrer.promo_status,
-      member_type: 'Full'
-    };
-
-    // Register new admin
-    this.createContentAdministrator(admin);
-  }
-
-  createContentAdministrator(admin) {
-
-    // for now assume no restaurant known, might change for different join modes
-
-    // test to break it...
-    // admin.member_language = 'kkkkkkk';
-    this.memberService.createAdministrator(admin).subscribe(
-      data => {
-        console.log(data);
-        // Check for duplicate administrator record
-        if (data['status'] === 'Duplicate') {
-          this.dspRegistrationResult('duplicate', data['error']);
-        } else {
-          // Successful registration
-          sessionStorage.setItem('referrer_type', this.referrer.type);
-
-          // TODO removed the 'success' snack bar display. Might need to consider showing it in the 'self' path?
-          // this.cmsLocalService.dspSnackbar(this.t_data.Success);
-
-          // If the member has been referred
-          // update the usage record and fast-track them
-          if (this.referrer.type === 'member') {
-            this.updateReferralUsage(data['member_id']);
-            this.autoSignIn(data['member_id']);
-          } else {
-            // Registration success
-            this.dspRegistrationResult('self-registered');
-          }
-
-        }
-        this.load.close();
-        this.isSubmitting = false;
-
-      },
-      error => {
-        console.log(JSON.stringify(error));
-        this.cmsLocalService.dspSnackbar(
-          `${this.t_data.Critical}`,
-          'OK',
-          20
-        );
-        // TODO more translations (or do we care since this is only relevant for support...)
-        this.appService.reportCriticalError('Failed to register new member!\n\n' +
-          'First Name : ' + admin.member_first_name + '\n\n' +
-          'Last Name : ' + admin.member_last_name + '\n\n' +
-          'Telephone : ' + admin.member_telephone + '\n\n' +
-          'Email : ' + admin.member_email).subscribe(emaildata => {
-            console.log(emaildata);
-            this.load.close();
-          },
-          emailerror => {
-            console.log(emailerror);
-            this.load.close();
-          });
-      });
-
-  }
+  // createContentAdministrator(admin) {
+  //
+  //   // for now assume no restaurant known, might change for different join modes
+  //
+  //   // test to break it...
+  //   // admin.member_language = 'kkkkkkk';
+  //   this.memberService.createAdministrator(admin).subscribe(
+  //     data => {
+  //       console.log(data);
+  //       // Check for duplicate administrator record
+  //       if (data['status'] === 'Duplicate') {
+  //         this.dspRegistrationResult('duplicate', data['error']);
+  //       } else {
+  //         // Successful registration
+  //         sessionStorage.setItem('referrer_type', this.referrer.type);
+  //
+  //         // TODO removed the 'success' snack bar display. Might need to consider showing it in the 'self' path?
+  //         // this.cmsLocalService.dspSnackbar(this.t_data.Success);
+  //
+  //         // If the member has been referred
+  //         // update the usage record and fast-track them
+  //         if (this.referrer.type === 'member') {
+  //           this.updateReferralUsage(data['member_id']);
+  //           this.autoSignIn(data['member_id']);
+  //         } else {
+  //           // Registration success
+  //           this.dspRegistrationResult('self-registered');
+  //         }
+  //
+  //       }
+  //       this.load.close();
+  //       this.isSubmitting = false;
+  //
+  //     },
+  //     error => {
+  //       console.log(JSON.stringify(error));
+  //       this.cmsLocalService.dspSnackbar(
+  //         `${this.t_data.Critical}`,
+  //         'OK',
+  //         20
+  //       );
+  //       // TODO more translations (or do we care since this is only relevant for support...)
+  //       this.appService.reportCriticalError('Failed to register new member!\n\n' +
+  //         'First Name : ' + admin.member_first_name + '\n\n' +
+  //         'Last Name : ' + admin.member_last_name + '\n\n' +
+  //         'Telephone : ' + admin.member_telephone + '\n\n' +
+  //         'Email : ' + admin.member_email).subscribe(emaildata => {
+  //           console.log(emaildata);
+  //           this.load.close();
+  //         },
+  //         emailerror => {
+  //           console.log(emailerror);
+  //           this.load.close();
+  //         });
+  //     });
+  //
+  // }
 
   autoSignIn(member_id) {
     // take the member directly to authentication - first need to re-read the member record to get the full object
@@ -265,7 +272,9 @@ export class JoinComponent implements OnInit {
   }
 
   dspRegistrationResult(resultType, errorType = null) {
+
     this.newRegResult = resultType;
+
     switch (resultType) {
       case 'duplicate': {
         console.log('Duplicate user:', errorType);
@@ -280,7 +289,7 @@ export class JoinComponent implements OnInit {
       }
       case 'self-registered': {
         this.cmsLocalService.dspSnackbar(
-          `${this.t_data.Check}`,
+          this.translate.instant('JOIN.msgCheckEmail'),
           'OK',
           10
         );
@@ -289,25 +298,25 @@ export class JoinComponent implements OnInit {
     }
   }
 
-  curationRequest() {
-    this.load.open(this.t_data.Sending);
-    // Format as markdown
-    const msg = `## ${this.t_data.Help}\n\n` +
-      `${this.t_data.Attempt}\n\n` +
-      `${this.t_data.ASAP}\n` +
-      ` - **${this.t_data.FullName}**: ${this.currentApplicant.name}\n` +
-      ` - **${this.t_data.Mobile}**: ${this.currentApplicant.mobile}\n` +
-      ` - **${this.t_data.Email}**: ${this.currentApplicant.email}\n` +
-      ` - **${this.t_data.Job}**: ${this.currentApplicant.role}\n`;
-
-    this.memberService.sendEmailRequest('curation', 'support', this.t_data.Problem, msg).subscribe(data => {
-        console.log(data);
-        this.newRegResult = 'support-request-sent';
-        this.load.close();
-    },
-      error => {
-        console.log(error);
-        this.load.close();
-      });
-  }
+  // curationRequest() {
+  //   this.load.open(this.t_data.Sending);
+  //   // Format as markdown
+  //   const msg = `## ${this.t_data.Help}\n\n` +
+  //     `${this.t_data.Attempt}\n\n` +
+  //     `${this.t_data.ASAP}\n` +
+  //     ` - **${this.t_data.FullName}**: ${this.currentApplicant.name}\n` +
+  //     ` - **${this.t_data.Mobile}**: ${this.currentApplicant.mobile}\n` +
+  //     ` - **${this.t_data.Email}**: ${this.currentApplicant.email}\n` +
+  //     ` - **${this.t_data.Job}**: ${this.currentApplicant.role}\n`;
+  //
+  //   this.memberService.sendEmailRequest('curation', 'support', this.t_data.Problem, msg).subscribe(data => {
+  //       console.log(data);
+  //       this.newRegResult = 'support-request-sent';
+  //       this.load.close();
+  //   },
+  //     error => {
+  //       console.log(error);
+  //       this.load.close();
+  //     });
+  // }
 }
