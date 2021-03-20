931eb86359de20b83b93e1884a1c97d6014dee32
jmbrnrd <jmbarnrd@gmail.com>
2021-03-20 12:30:28 +0000

    pk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_URL: 'https://rc-server-prod.herokuapp.com',
   // TODO: Update
-  stripe_key: 'sk_test_51HwTj0DK2S86a4QiuwBhM5GmkCpjMpDy0Q3R5lfpjxjksLaIbp6vPPtlyeSFEqiMfrYMDK4QbzG9Al04LqP6Lfju00Ailg2JkI'
+  stripe_key: 'pk_test_51HwTj0DK2S86a4QiDZ7hvjuiwLjWSZU3Yy5LFYnBrEDHNsCF1nbwrXOzCOPPDV0cxHvZIC5m5TZsbWoAztS4ZCnE00nKRGn4jh'
 };
