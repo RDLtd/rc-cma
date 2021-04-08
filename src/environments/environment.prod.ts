afde05d5a2c8b6e3ef723fab501b4730b50ccd3c
jmbrnrd <jmbarnrd@gmail.com>
2021-04-08 08:20:55 +0100

    Routing updates: simplified the cms routing and replaces 'profile' with settings all to reflect our new 'Hub' centred approach iin the GA.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //API_URL: 'https://rc-server-prod.herokuapp.com',
-  API_URL: 'https://rc-server-staging.herokuapp.com',
+  API_URL: 'https://rc-server-prod.herokuapp.com',
+  //API_URL: 'https://rc-server-staging.herokuapp.com',
   stripe_key: 'pk_test_51HwTj0DK2S86a4QiDZ7hvjuiwLjWSZU3Yy5LFYnBrEDHNsCF1nbwrXOzCOPPDV0cxHvZIC5m5TZsbWoAztS4ZCnE00nKRGn4jh'
 };
