eac48be9f57f8aef85c170af419078ba71b8cc90
James Barnard <jmbarnard@gmail.com>
2021-05-11 07:30:49 +0100

    RI prep

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //API_URL: 'https://rc-server-prod.herokuapp.com',
-  API_URL: 'https://rc-server-staging.herokuapp.com',
+  API_URL: 'https://rc-server-prod.herokuapp.com',
+  //API_URL: 'https://rc-server-staging.herokuapp.com',
   rc_stripe_key: 'pk_live_51HwTj0DK2S86a4Qia8CGKD9o1mmQ474tUk3v3vJXyiIXNRdZF2eFPQYmTas5khh2Y5oIGgCTG4D4gyJJ2FB3HHIK00mTDK1LbI',
   ri_stripe_key:
     'pk_live_51IiLuvFvEMJjooglF2wr0sl7S1B0hltom6GweN7Mce5o8qrUG2bibXa2lPzg99oTToe6IkWNhLnXtgJJHQ4WIFFS00lCsNM7eY'
