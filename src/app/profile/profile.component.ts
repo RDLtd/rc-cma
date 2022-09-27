660cac6c2e6bea7d7ade6865058bffb9aa99b685
James Barnard <jmbarnard@gmail.com>
2022-09-27 15:13:27 +0100

    Exteranlise MZ js

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfig } from '../app.config';
+import { LoadService } from '../common';
+import { HeaderService } from '../common/header.service';
 
-/**
- * Reference the Mozrest global object loaded
- * via src/assets
- */
-declare var MOZ: any;
+declare var mozrest: any;
 
 @Component({
   selector: 'app-rc-profile',
@@ -17,17 +15,30 @@ export class ProfileComponent implements OnInit {
   mozKey: string;
   moz: any;
 
-  constructor(private config: AppConfig) {
+  constructor(
+      private loader: LoadService,
+      private config: AppConfig,
+      private headerService: HeaderService
+  ) {
+    this.loader.open();
+    this.headerService.updateSectionName('Profile Management');
     this.mozId = config.mozId;
     this.mozKey = config.mozKey;
   }
 
   ngOnInit(): void {
-    // Initialise the MozRest component
-    this.moz = MOZ.init({
-      token: this.mozKey,
-      venueId: '123'
+
+    console.log(mozrest);
+
+    this.moz = mozrest.init('mz-sdk', {
+      partnerId: this.mozId,
+      accessKey: '6332b895324cce9732382433',
+      venueId: '6332b895324cce9732382433'
     });
+
+    this.moz.showPage('Listing');
+    this.loader.close();
+
   }
 
 }
