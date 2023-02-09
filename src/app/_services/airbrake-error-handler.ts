b54c724961346c464018ec85478a12f3b1772c3e
KenStratford <ks@ongava.com>
2023-02-09 18:44:37 +0200

    You might need this...

---
+++ src/app/_services/airbrake-error-handler.ts
@@ -0,0 +1,20 @@
+
+// src/app/airbrake-error-handler.ts
+
+import { ErrorHandler } from '@angular/core';
+import { Notifier } from '@airbrake/browser';
+
+export class AirbrakeErrorHandler implements ErrorHandler {
+  airbrake: Notifier;
+
+  constructor() {
+    this.airbrake = new Notifier({
+      projectId: 480741,
+      projectKey: '1fdebb905739780366a931460fb6958c',
+      environment: 'production'
+    });
+  }
+
+  handleError(error: any): void {
+  }
+}
