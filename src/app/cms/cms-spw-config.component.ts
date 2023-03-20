0355cac67994a03840b9ae92a830cf2088d768fe
James Barnard <jmbarnard@gmail.com>
2023-03-20 12:10:12 +0000

    config and themes

--- src/app/cms/cms-spw-config.component.ts
+++ src/app/cms/cms-spw-config.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CmsLocalService } from './cms-local.service';
 import { HelpService } from '../common';
 import { AnalyticsService, CMSService } from "../_services";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Restaurant } from '../_models';
 import {AppConfig} from "../app.config";
 
@@ -23,29 +23,20 @@ export class CmsSpwConfigComponent implements OnInit {
     'collective',
     'carshalton',
     'rdl'
-  ]
+  ];
+  selectedTheme = 'apptiser'
+
+  configFormGroup: FormGroup;
 
   theme_id: number;
-  website_options: {};
+
+  websiteConfig: {};
 
   isChecked = true;
-  formGroup = this._formBuilder.group({
-    showImageGallery: [true],
-    showHtmlMenu: [true, Validators.requiredTrue],
-    showDownloadMenus: [true],
-    showReservations: [true],
-    showBookingWidget: [true],
-    showGroupBookings: [false],
-    showPrivateDining: [false],
-    showContacts: [true],
-    showLinks: [true],
-    showParking: [true],
-    showMap: [true],
-    showTransport: [true],
-  });
 
   constructor(
     private cmsLocalService: CmsLocalService,
+    private fb: FormBuilder,
     public help: HelpService,
     private cms: CMSService,
     private _formBuilder: FormBuilder,
@@ -64,8 +55,9 @@ export class CmsSpwConfigComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.cssThemes);
+
     this.loadThemes();
+    this.generateConfigForm();
     this.cmsLocalService.getRestaurant()
       .subscribe({
       next: data => {
@@ -76,6 +68,55 @@ export class CmsSpwConfigComponent implements OnInit {
     });
   }
 
+  setConfig(): void {
+    Object.entries(this.websiteConfig).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+      this.configFormGroup.get(key).setValue(value);
+    })
+  }
+
+  generateConfigForm(): void {
+    this.configFormGroup = this.fb.group({
+      showOpeningNotes: true,
+      showImageGallery: [true],
+      // menus
+      showHtmlMenu: [true],
+      showDownloadMenus: [true],
+      // reservations
+      showReservations: [true],
+      showReservationsInfo: [{value: true, disabled: false}],
+      showBookingWidget: [{value: true, disabled: false}],
+      showGroupBookings: [{value: false, disabled: false}],
+      showPrivateDining: [{value: false, disabled: false}],
+      // contacts
+      showContacts: [true],
+      showLinks: [{value: true, disabled: false}],
+      showParking: [{value: true, disabled: false}],
+      showTransport: [{value: true, disabled: false}],
+      // location
+      showMap: [true]
+    });
+  }
+
+  // TODO: JB: I'm sure this would be better done using nested form groups
+  //  but I don't have time to investigate
+  toggleSection(control): void {
+    const disable = !this.configFormGroup.get(control).value;
+    let controls: string[];
+    if (control === 'showReservations') {
+      controls = ['showReservationsInfo', 'showBookingWidget', 'showGroupBookings', 'showPrivateDining'];
+      controls.forEach( c => {
+        disable ? this.configFormGroup.get(c).disable() : this.configFormGroup.get(c).enable();
+      })
+    }
+    if (control === 'showContacts') {
+      controls = ['showLinks', 'showParking', 'showTransport'];
+      controls.forEach( c => {
+        disable ? this.configFormGroup.get(c).disable() : this.configFormGroup.get(c).enable();
+      })
+    }
+  }
+
   getConfig(): void {
     // get the website config data for this restaurant
     this.cms.getWebConfig(Number(this.restaurant.restaurant_id))
@@ -83,8 +124,9 @@ export class CmsSpwConfigComponent implements OnInit {
         next: data => {
           // map data to local
           this.theme_id = data['website_config'].website_config_theme_id;
-          this.website_options = data['website_config'].website_config_options;
-          console.log(this.theme_id, this.website_options);
+          this.websiteConfig = data['website_config'].website_config_options;
+          this.setConfig();
+          console.log('Config', this.websiteConfig);
         },
         error: error => {
           console.log('getWebConfig', error);
@@ -102,12 +144,11 @@ export class CmsSpwConfigComponent implements OnInit {
     // Note that association check is done at the back end, which check for ANY member having this restaurant associated
 
     // for testing only!
-    const restaurant_id = '92596';
     const membership_type = 'standard';
 
     // TODO make sure we have the options loaded... Now we have 'theme': 'aaaaaaaa' in website_options.
 
-    this.cms.publish(this.restaurant.restaurant_id, production, membership_type, this.website_options)
+    this.cms.publish(this.restaurant.restaurant_id, production, membership_type, this.websiteConfig)
       .then(res => {
 
         if (res['status'] !== 'OK') {
@@ -147,8 +188,8 @@ export class CmsSpwConfigComponent implements OnInit {
   }
 
   // Web config
-  alertFormValues(formGroup: FormGroup): void {
-    alert(JSON.stringify(formGroup.value, null, 2));
+  submitFormValues(formGroup: FormGroup): void {
+    console.log(formGroup.value);
   }
 
   // Domains
